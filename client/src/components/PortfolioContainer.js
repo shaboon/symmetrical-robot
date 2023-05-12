99f0d4f8523d6695281417618e17ccb4783a5735
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-05-12 12:35:26 -0400

    folder structure reset for MERN standard

--- client/src/components/PortfolioContainer.js
+++ client/src/components/PortfolioContainer.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import React, { useState, useEffect } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import Characters from "./pages/Characters";
+import Lists from "./pages/Lists";
+import Game from "./pages/Game";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  useEffect(() => {
+    document.title = "Interstellar Index";
+  }, []);
 
   // TODO: Add a comment describing the functionality of this method
   const renderPage = () => {
-    if (currentPage === 'Home') {
+    if (currentPage === "Home") {
       return <Home />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === "Characters") {
+      return <Characters />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === "Lists") {
+      return <Lists />;
     }
-    return <Contact />;
+    return <Game />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
