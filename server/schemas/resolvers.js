4800b27a9e7aa7454e65a4e516a93eeb2469602a
Sean Noway <sean.m.holian@gmail.com>
2023-05-12 14:13:49 -0400

    finished out server side JWT

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Profile } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    profiles: async () => {
+      return Profile.find();
+    },
+
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    addProfile: async (parent, { username, password }) => {
+      const profile = await Profile.create({ username, password });
+      const token = signToken(profile);
+
+      return { token, profile };
+    },
+    login: async (parent, { username, password }) => {
+      const profile = await Profile.findOne({ username });
+
+      if (!profile) {
+        throw new AuthenticationError('No profile with this username found!');
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+
+    // Set up mutation so a logged in user can only remove their profile and no one else's
+    removeProfile: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOneAndDelete({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
