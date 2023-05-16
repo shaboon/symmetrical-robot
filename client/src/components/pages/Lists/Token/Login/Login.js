ba31d8b046a208b571726b07e48f393411c2564c
Sean Noway <sean.m.holian@gmail.com>
2023-05-16 11:43:00 -0400

    testing routes for login

--- client/src/components/pages/Lists/Token/Login/Login.js
+++ client/src/components/pages/Lists/Token/Login/Login.js
@@ -58,16 +58,14 @@ const Login = (props) => {
                   placeholder="Your username"
                   name="username"
                   type="text"
-                  value={formState.username}
-                  onChange={handleChange}
+                  onChange={handleChange} 
                 />
                 <input
                   className="form-input"
                   placeholder="******"
                   name="password"
                   type="password"
-                  value={formState.password}
-                  onChange={handleChange}
+                  onChange={handleChange} 
                 />
                 <button
                   className="btn btn-block btn-info"
