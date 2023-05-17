84c9d41091c7bd7a6fd558ce9f99d91f601d30d9
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-05-17 15:23:42 -0400

    modified connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/my_database', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/interstellar-index', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
