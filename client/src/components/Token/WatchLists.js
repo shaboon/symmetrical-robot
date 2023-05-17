6e1f437591af9031f513d4c1ae6ea21f93bbbfad
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-05-17 14:36:27 -0400

    added non-func button to watch page

--- client/src/components/Token/WatchLists.js
+++ client/src/components/Token/WatchLists.js
@@ -4,12 +4,21 @@ import { ListData } from "./ListData";
 export default function WatchLists() {
   return ListData.map((list, index) => {
     return (
-      <div className="d-flex justify-content-center">
-        <h1>{list.name}</h1>
-        <div className="list" key={index}>
-          <ul>
+      <div className="container d-flex flex-column justify-content-center col-12 col-xl-4">
+        <div className="col-12 row">
+          <h1 className="col-12 row">{list.name}</h1>
+        </div>
+        <div className="list col-12 row" key={index}>
+          <ul className="col-12 row">
             {list.movies.map((movie, index) => {
-              return <li key={index}>{movie}</li>;
+              return (
+                <li key={index} className="col-12">
+                  <h2 className="">{movie}</h2>
+                  <button className="btn btn-primary">
+                    Remove From Watchlist
+                  </button>
+                </li>
+              );
             })}
           </ul>
         </div>
