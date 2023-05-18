8edcc378c8262a6c2eb8d3956f3e86ed99ccb1e2
anthonylieu <38957648+anthonylieu@users.noreply.github.com>
2023-05-18 02:39:32 -0400

    updated to correct connection.js, need to whitelist IP next

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const DB_URI = isProduction 
   ? process.env.MONGODB_URI 
-  : 'mongodb://localhost/interstellar-index';
+  : 'mongodb://anthonylieu:gatomagnet@cluster0.mongodb.net/interstellar-index';
 
 mongoose.connect(DB_URI, {
   useNewUrlParser: true,
