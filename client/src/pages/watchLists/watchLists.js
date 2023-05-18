14042f889a269a1852de4ab36e06caef6a47d5df
Sean Noway <sean.m.holian@gmail.com>
2023-05-18 10:43:18 -0400

    watchlist seeds

---
+++ client/src/pages/watchLists/watchLists.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Request from "../../components/Token/Request";
+
+import "./watchLists.css";
+
+export default function Contact() {
+
+return (
+<div className="container justify-content-center my-5">
+    <div className="row">
+        <div className="col-12">
+            <h1 className="text-center">Watch Lists</h1>
+        </div>  
+    </div>
+    <div className="row">
+        <div className="col-12">
+            <Request />
+        </div>
+    </div>
+</div>
+);
+}
+
+
+    
+  
